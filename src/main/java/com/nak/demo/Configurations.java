8338aeeebfe2c0828b4cbb7af11d11a5a24ae169
寺中　元希 <gteranak@MBA-11UAL-012.local>
2017-10-29 15:55:17 +0900

    add databases with error

---
+++ src/main/java/com/nak/demo/Configurations.java
@@ -0,0 +1,29 @@
+package com.nak.demo;
+
+import javax.persistence.*;
+
+@Entity
+public class Configurations {
+
+    @Id
+    private String name;
+
+    private String value;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
---
+++ src/main/java/com/nak/demo/DatabaseApplivation.java
@@ -0,0 +1,11 @@
+package com.nak.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+public class DatabaseApplivation {
+
+}
