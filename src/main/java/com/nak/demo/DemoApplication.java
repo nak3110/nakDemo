d278c50563ff7f9b5b82b8e28c5c469f6196bb62
寺中　元希 <gteranak@MBA-11UAL-012.local>
2017-10-29 16:19:05 +0900

    add databases (require entityManagerFactory)

--- src/main/java/com/nak/demo/DemoApplication.java
+++ src/main/java/com/nak/demo/DemoApplication.java
@@ -1,10 +1,15 @@
 package com.nak.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -20,4 +25,24 @@ class GreetingController {
 	String hello(@PathVariable String name) {
 		return "Hello, " + name + "!";
 	}
+}
+
+@Controller
+@EnableAutoConfiguration
+class DatabaseController {
+
+	@Autowired
+	ConfigurationsRepository configurationsRepository;
+
+	@RequestMapping("/")
+	public String home() {
+		System.out.println("[START] データベースに接続してデータを取得します。");
+		Page<Configurations> configs = configurationsRepository.findAll(new PageRequest(0, 10));
+		for (Configurations config : configs) {
+			System.out.println(config.getName() + " = " + config.getValue());
+		}
+		System.out.println("[END  ] データベースに接続してデータを取得します。");
+		return "index";
+	}
+
 }
